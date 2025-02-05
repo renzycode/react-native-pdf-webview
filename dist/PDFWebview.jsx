9c5f1408fcc78f9d0f991f342435ec2015f3f821
renzycode <93112325+renzycode@users.noreply.github.com>
2025-02-06 06:46:20 +0800

    updated

---
+++ dist/PDFWebview.jsx
@@ -0,0 +1,22 @@
+import { View, Dimensions } from "react-native";
+import { WebView } from "react-native-webview";
+
+export default function PDFWebView({src}) {
+    const deviceWidth = Dimensions.get("window").width;
+    console.log("src: ", `https://linen-okapi-882592.hostingersite.com/react-native-pdf-viewer/api.php?source=${src}`);
+    return (
+        <View style={{ flex: 1, backgroundColor: "#f4f4f4" }}>
+            <WebView
+                source={{ uri: `https://linen-okapi-882592.hostingersite.com/react-native-pdf-viewer/api.php?source=${src}` }}
+                style={{ flex: 1 }}
+                scalesPageToFit={true}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+                injectedJavaScript={`
+                    document.body.style.width = '${deviceWidth}px !important';
+                    document.body.style.overflowX = 'hidden'; 
+                `}
+            />
+        </View>
+    );
+}
